d7dddd8cd2e3e2ada01163c9c56e36c91239d5ef
Christina <christinachon90@gmail.com>
2019-10-22 19:10:08 -0700

    added padding to search bar

--- client/src/App.js
+++ client/src/App.js
@@ -18,11 +18,10 @@ class App extends Component {
         {/* <Content /> */}
         {/* <Item /> */}
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This is where to put the movie information.
         </p>
         <Footer />
       </div>
