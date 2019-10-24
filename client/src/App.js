c421bb0fe997c8281028319b7171253753e9f881
Huy Nguyen <huy.m.nguyen@live.com>
2019-10-23 20:26:12 -0700

    Add home and saved page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,32 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Search from './components/Search';
 import Content from './components/Content';
 import Item from './components/Item';
+import Home from "./pages/Home";
+import Saved from "./pages/Saved";
 import Footer from './components/Footer';
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Header />
-        <Search />
-        {/* <Content /> */}
-        {/* <Item /> */}
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Header />
+          <Search />
+          {/* <Content /> */}
+          {/* <Item /> */}
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/Saved" component={Saved} />
+          </Switch>
+          <Footer />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Footer />
-      </div>
+      </Router>
     );
   }
 }
