5c2b49eafd14b2f00ee44cb98369c6ed79d0b3b9
Christina <christinachon90@gmail.com>
2019-10-23 20:26:27 -0700

    updated card

--- client/src/components/Item/Item.js
+++ client/src/components/Item/Item.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import './Item.css';
 
-function Item(props) {
+function Item() {
     return (
 
         <div className="card">
-            <img src ="http://catalog.osaarchivum.org/assets/thumbnail_placeholder_movie-480596e192e7043677f77cf78b13bdd1.jpg"></img>
-            <p><strong>Movie Title Here</strong></p>
-            <p>This is a description of the movie. It is a great movie.</p>
-             <i className="fas fa-film"></i>
-             <i className="far fa-trash-alt"></i>
+            <img
+            className="card-img-top"
+            src="https://www.sunnxt.com/images/placeholders/placeholder_vertical.gif"
+            alt="Card image cap"></img>
+            <div className="card-body">
+                <h5 className="card-title">Movie Title</h5>
+                <p className="card-text">This is the movie description. What a great movie.</p>
+                <button
+                href="#"
+                className="btn btn-secondary">Add to List</button>
+            </div>
         </div>
 
-        
-            );
-          }
-          
-          
+    );
+}
+
+
 export default Item;
