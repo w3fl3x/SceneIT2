1a86ac22addfb30c7d18a47ab4681187b922cd5d
Huy Nguyen <huy.m.nguyen@live.com>
2019-10-26 09:56:34 -0700

    Add fixed-bottom to footer className.

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 
 const Footer = () => (
     <footer className='footer fixed-bottom'>
-        <div className='footer-bottom'>
+        <div className='footer-bottom fixed-bottom'>
             UW Coding Bootcamp 2019
         </div>
     </footer>
