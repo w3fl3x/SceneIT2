9b44abcb887e38cb090a6c4dd6f0330078d4d73a
Huy Nguyen <huy.m.nguyen@live.com>
2019-10-28 18:38:31 -0700

    Made invisible burger button visible to user. Removed codes that was not needed in and cleaned up comments.

--- client/src/components/Content/Content.js
+++ client/src/components/Content/Content.js
@@ -1,34 +1,31 @@
 import React, { Component } from "react";
+import { ButtonToolbar, ToggleButton, ToggleButtonGroup } from "react-bootstrap";import { Container, Row, Col } from "../Grid";
 import './Content.css';
-import { Container, Row, Col } from "../Grid";
-import { ButtonToolbar, ToggleButton, ToggleButtonGroup } from "react-bootstrap";
 
 const Content = () => (
+    
+    
     <>
-        <p>Stuff</p>
-
         <Container>
+            <div className="movieList">
             <Row>
                 <Col size="md-12">
 
                     <ButtonToolbar>
                         <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
-                            <ToggleButton value={1}>Top Movies</ToggleButton>
-                            <ToggleButton value={2}>Seen</ToggleButton>
+                            <ToggleButton variant="outline-secondary" value={1}>Must Watch</ToggleButton>
+                            <ToggleButton variant="outline-secondary" value={2} >Seen It</ToggleButton>
                         </ToggleButtonGroup>
                     </ButtonToolbar>
-
-                    {/* <Button onClick={this.handleFormSubmit} type="success" className="input-lg">
-                    Search
-                </Button>
-                <Button onClick={this.handleFormSubmit} type="success" className="input-lg">
-                    Search
-                </Button> */}
                 </Col>
             </Row>
-            {/* <Row>
-
-        </Row> */}
+            <Row>
+                <p>Movie List Here</p>
+                <Col size="md-5">
+                    
+                </Col>
+            </Row>
+            </div>
         </Container>
     </>
 );
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,98 +3,63 @@ import { Link } from "react-router-dom";
 import './Navbar.css';
 
 class Navbar extends Component {
-    state = {
-      open: false,
-      width: window.innerWidth
-    };
-  
-    updateWidth = () => {
-      const newState = { width: window.innerWidth };
-  
-      if (this.state.open && newState.width > 991) {
-        newState.open = false;
-      }
-  
-      this.setState(newState);
-    };
-  
-    toggleNav = () => {
-      this.setState({ open: !this.state.open });
-    };
-  
-    componentDidMount() {
-      window.addEventListener("resize", this.updateWidth);
-    }
-  
-    componentWillUnmount() {
-      window.removeEventListener("resize", this.updateWidth);
+  state = {
+    open: false,
+    width: window.innerWidth
+  };
+
+  updateWidth = () => {
+    const newState = { width: window.innerWidth };
+
+    if (this.state.open && newState.width > 991) {
+      newState.open = false;
     }
 
-    render() {
-        return(
-          <nav className='navbar navbar-expand-lg fixed-top'>
-            <Link className="name" to="/">
-              SceneIT2
+    this.setState(newState);
+  };
+
+  toggleNav = () => {
+    this.setState({ open: !this.state.open });
+  };
+
+  componentDidMount() {
+    window.addEventListener("resize", this.updateWidth);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWidth);
+  }
+
+  render() {
+    return(
+      <nav className='navbar navbar-expand-lg fixed-top'>
+        <Link className="name" to="/">
+          SceneIT2
+        </Link>
+        <button className="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="navbar-link collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto w-100 justify-content-end">
+            <Link
+              onClick={this.toggleNav}
+              className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}
+              to="/"
+              >
+              Home
             </Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="navbar-link collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav mr-auto w-100 justify-content-end">
-                <Link
-                  onClick={this.toggleNav}
-                  className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}
-                  to="/"
-                  >
-                  Home
-                </Link>
-                <Link
-                  onClick={this.toggleNav}
-                  className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}
-                  to="/saved"
-                  >
-                  Saved
-                </Link>
-              </ul>
-            </div>
-            
-            {/* <button
-                onClick={this.toggleNav}
-                className="navbar-toggler"
-                data-toggle="collapse"
-                data-target="#navbarNav"
-                aria-controls="navbarNav"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                <span className="navbar-toggler-icon" />
-            </button>
-            <div className={`${this.state.open ? "" : "collapse "}navbar-collapse`} id="navbarNav">
-                <ul className="navbar-nav">
-                <li className="nav-item">
-                    <Link
-                    onClick={this.toggleNav}
-                    className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}
-                    to="/"
-                    >
-                    Home
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link
-                    onClick={this.toggleNav}
-                    className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}
-                    to="/saved"
-                    >
-                    Saved
-                    </Link>
-                </li>
-                </ul>
-            </div> */}
-
-          </nav>
-        )
-    }
+            <Link
+              onClick={this.toggleNav}
+              className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}
+              to="/saved"
+              >
+              Saved
+            </Link>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
 }  
 
 export default Navbar;
