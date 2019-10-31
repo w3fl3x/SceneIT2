2a9ff5a5a45fd471a03dc1954103db2f7e54890c
Huy Nguyen <huy.m.nguyen@live.com>
2019-10-30 18:18:04 -0700

    Changed Login button to User.

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -9,7 +9,7 @@ class Login extends Component {
         <>
             <div className="btn-group" role="group">
                 <button id="btnGroupDrop1" type="button" className="btn btn-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Log In
+                User
                 </button>
                 <div className="dropdown-menu" aria-labelledby="btnGroupDrop1">
                     <div className="fb-login-button" data-width="" data-size="small" data-button-type="login_with" data-auto-logout-link="true" data-use-continue-as="true"></div>
