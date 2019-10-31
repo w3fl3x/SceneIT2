19c8b5fa56de5faf761613b2c04708fa6da5d456
AAVA <aav.angeles@gmail.com>
2019-10-30 21:51:29 -0700

    Got API working

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,6 +3,6 @@ import axios from "axios";
 
 export default {
     getMovies: function(query) {
-    return axios.get("https://api.themoviedb.org/3/search/movie?api_key=254cd713084221bba3f79e9929c4662b&language=en-US", { params: { q: query } });
+    return axios.get("https://api.themoviedb.org/3/search/movie?api_key=254cd713084221bba3f79e9929c4662b&language=en-US&query=" + query)
   }
 };
