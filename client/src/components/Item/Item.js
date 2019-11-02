b5b8f98b30b52c057351636a3675280641c08830
winTS <huynhts@gmail.com>
2019-11-02 13:13:41 -0700

    fixed card sizes with class name

--- client/src/components/Item/Item.js
+++ client/src/components/Item/Item.js
@@ -1,29 +1,32 @@
-import React from 'react';
-import './Item.css';
+import React from "react";
+import "./Item.css";
 import { Container, Row, Col } from "../Grid";
 
 function Item(props) {
-    return (
-         <Row>
-            <Col size="md-4" className="itemCard">
-                <div className="card">
-                    <img
-                    className="card-img-top"
-                    src={"https://image.tmdb.org/t/p/w185_and_h278_bestv2/" + props.posterPath}
-                    alt="Card image cap"></img>
-                    <div className="card-body">
-                        <h5>{props.title}</h5>
-                        <p>Rating: {props.voteAverage}/10</p>
-                        <p>{props.releaseDate}</p>
-                        <button
-                        href="#"
-                        className="btn btn-secondary">Add to List</button>
-                    </div>
-                </div>
-            </Col>
-        </Row>   
-    );
+  return (
+    <Row>
+      <Col size="md-4" className="itemCard">
+        <div className="card cardResults">
+          <img
+            className="card-img-top"
+            src={
+              "https://image.tmdb.org/t/p/w185_and_h278_bestv2/" +
+              props.posterPath
+            }
+            alt="Card image cap"
+          ></img>
+          <div className="card-body">
+            <h5>{props.title}</h5>
+            <p>Rating: {props.voteAverage}/10</p>
+            <p>{props.releaseDate}</p>
+            <button href="#" className="btn btn-secondary">
+              Add to List
+            </button>
+          </div>
+        </div>
+      </Col>
+    </Row>
+  );
 }
-
 
 export default Item;
